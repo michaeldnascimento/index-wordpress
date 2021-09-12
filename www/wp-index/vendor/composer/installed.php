03391e6db6269efbf40bd211cac46cc060ae13af
Michael D. Nascimento <michaeldnascimento@hotmail.com>
2021-09-12 09:21:02 -0300

    Criando pasta, para listagem de produtos

---
+++ www/wp-index/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3c9e0ca8bb73265a4aafad46d1b2097680ad9829',
+    'name' => 'wp-index/index',
+  ),
+  'versions' => 
+  array (
+    'wp-index/index' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3c9e0ca8bb73265a4aafad46d1b2097680ad9829',
+    ),
+  ),
+);
